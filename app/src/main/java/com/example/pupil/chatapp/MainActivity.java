117aa5e7efbcec595218c5b1ab461637afec5807
mkleinegger <mkleinegger@gmail.com>
2019-04-11 08:36:45 +0200

    Initial Checkin

--- app/src/main/java/com/example/pupil/chatapp/MainActivity.java
+++ app/src/main/java/com/example/pupil/chatapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // ein toller Kommentars
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
