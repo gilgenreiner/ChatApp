d52b24347aeac7f62763a01f889761e9a5fff052
gilgenreiner <d.gilgenreiner@gmail.com>
2019-04-28 22:52:53 +0200

    DeleteProfile

--- app/src/main/java/at/htl_villach/chatapplication/popupDeleteDialogActivity.java
+++ app/src/main/java/at/htl_villach/chatapplication/popupDeleteDialogActivity.java
@@ -10,12 +10,21 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class popupDeleteDialogActivity extends AppCompatActivity {
-
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    String userId ;
+    String email ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,17 +37,51 @@ public class popupDeleteDialogActivity extends AppCompatActivity {
         int heigth = dm.heightPixels;
 
         getWindow().setLayout((int) (width*1), (int) (heigth*0.3));
-
+        firebaseAuth = FirebaseAuth.getInstance().getInstance();
     }
 
     public void deleteProfile(View view) {
+
         final TextInputLayout txtPassword = findViewById(R.id.txtPassword);
         String password = txtPassword.getEditText().getText().toString();
         if( TextUtils.isEmpty(password)) {
             Toast.makeText(getApplicationContext(), R.string.emptyPasswordField, Toast.LENGTH_SHORT).show();
         } else {
-            Intent intent = new Intent(popupDeleteDialogActivity.this, LoginActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            user = firebaseAuth.getCurrentUser();
+            userId = user.getUid();
+            email = user.getEmail();
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        DatabaseReference drfUser = FirebaseDatabase.getInstance().getReference("Users").child(userId);
+                                        drfUser.removeValue();
+                                        Toast.makeText(popupDeleteDialogActivity.this, "User account deleted!",
+                                                Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(popupDeleteDialogActivity.this, LoginActivity.class);
+                                        intent.putExtra("allowBack", false);
+                                        startActivity(intent);
+                                    }else{
+                                        Toast.makeText(popupDeleteDialogActivity.this, "Something went wrong!",
+                                                Toast.LENGTH_SHORT).show();
+                                        onBackPressed();
+                                    }
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), "Something went wrong " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
         }
     }
 
